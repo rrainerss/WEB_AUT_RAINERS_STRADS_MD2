da67b18be72c7d02038ec4e38a8448a558227448
rrainerss <rainersstrads8@gmail.com>
2024-06-02 22:15:32 +0300

    Small change

--- cypress/e2e/qa/appointment_history.cy.js
+++ cypress/e2e/qa/appointment_history.cy.js
@@ -17,6 +17,8 @@ describe("Appointment History", () => {
         cy.get('#menu-toggle').click();
         cy.get('#sidebar-wrapper').should('have.class', 'active');
         cy.get(':nth-child(4) > a').click();
+
+        //Body content
         cy.get('.col-sm-12 > p').should('be.visible').and('have.text', 'No appointment.');
     });
     Cypress.on("uncaught:exception", (err, runnable) => {
